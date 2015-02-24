6422aad6c73f4e23a6eac8af95fafca9cfc40b5f
Greg Ochab <g.ochab@yoursimpletruth.com>
2015-02-24 10:04:55 -0600

    Integrate neat theme
    http://themeforest.net/item/neat-one-page-html5-with-sass-bourbon-neat/8
    646432

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,14 @@
-<footer>
+<footer class="footer">
+	<div class="container">
 
+		<p class="copyright">Copyright &copy; 2014 <a href="index.html">Neat</a> - All Rights Reserved.</p>
+
+		<ul class="social">
+			<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+			<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+			<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+			<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+		</ul>
+
+	</div>
 </footer>
--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,41 @@
-<header>
-	<a href="/" id="logo">Client Name</a>
+<header id="navigation" class="navigation headroom">
+<div class="navigation-wrapper">
+
+	<!-- logo - you can put yours here -->
+	<a href="/" class="logo"><img src="img/logo.png" alt="logo"></a>
+
+	<!-- mobile menu toggle -->
+	<a href="" class="navigation-menu-button" id="js-mobile-menu">MENU</a>
+
+	<!-- navigation menu -->
+	<div class="nav">
+  		<ul id="navigation-menu">
+  			<li class="nav-link active"><a href="#section-intro">Home</a></li>
+	        <li class="nav-link"><a href="#section-features">Features</a></li>
+	        <li class="nav-link"><a href="#section-showcase">Showcase</a></li>
+	        <li class="nav-link"><a href="#section-blog">Blog</a></li>
+	        <li class="nav-link"><a href="#section-contact">Contact</a></li>
+
+			 <li class="nav-link more"><a href="javascript:void(0)">More</a>
+	          <ul class="submenu">
+	            <li><a href="javascript:void(0)">Submenu Item</a></li>
+	            <li><a href="javascript:void(0)">Another Item</a></li>
+	            <li class="more"><a href="javascript:void(0)">Item with submenu</a>
+	              <ul class="submenu">
+	                <li><a href="javascript:void(0)">Sub-submenu Item</a></li>
+	                <li><a href="javascript:void(0)">Another Item</a></li>
+	              </ul>
+	            </li>
+	            <li class="more"><a href="javascript:void(0)">Another submenu</a>
+	              <ul class="submenu">
+	                <li><a href="javascript:void(0)">Sub-submenu</a></li>
+	                <li><a href="javascript:void(0)">An Item</a></li>
+	              </ul>
+	            </li>
+	          </ul>
+	        </li>
+  		</ul>
+	</div>
+
+	</div>
 </header>
